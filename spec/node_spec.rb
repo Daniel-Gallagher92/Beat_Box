8cdffaef049c5ed49dc2ba43ea656cc28f08adb0
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-11 18:26:58 -0600

    Tests next_node

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,12 +3,17 @@ require 'RSpec'
 
 RSpec.describe Node do 
   it "exists" do 
-    node = Node.new("plop")
+    node = Node.new("plop", nil)
+    next_node = nil
     expect(node).to be_an_instance_of(Node)
   end
  it "has data" do 
-  node = Node.new("plop")
+  node = Node.new("plop", nil)
+  next_node = nil
   expect(node.data).to eq("plop")
  end
-
+ it "has next_node" do 
+  node = Node.new("plop", nil)
+  expect(node.next_node).to eq(nil)
+ end
 end
