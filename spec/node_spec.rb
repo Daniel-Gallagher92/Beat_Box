7cd70da5b0e30d5bfa3d8542c3fd1691c146b5f8
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-11 18:30:02 -0600

    Minor test refactor, removed unnecessary setup step

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,12 +4,10 @@ require 'RSpec'
 RSpec.describe Node do 
   it "exists" do 
     node = Node.new("plop", nil)
-    next_node = nil
     expect(node).to be_an_instance_of(Node)
   end
  it "has data" do 
   node = Node.new("plop", nil)
-  next_node = nil
   expect(node.data).to eq("plop")
  end
  it "has next_node" do 
