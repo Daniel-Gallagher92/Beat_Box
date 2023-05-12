187546c05c590638fef14b42c26eb8cd097bcab9
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-11 19:52:33 -0600

    Test nil head

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -3,7 +3,11 @@ require './lib/node'
 
 RSpec.describe Linked_list do 
   it "exists" do 
-    list = Linked_list.new()
+    list = Linked_list.new(nil)
     expect(list).to be_an_instance_of(Linked_list)
+  end
+  it "has nil head" do 
+    list = Linked_list.new(nil)
+    expect(list.head).to eq(nil)
   end
 end
