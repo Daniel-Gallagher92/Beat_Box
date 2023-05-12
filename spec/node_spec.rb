c31340c486505d8148a9fc2e73cf0cacb39e7572
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-11 18:22:21 -0600

    Tests has data

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,8 +3,12 @@ require 'RSpec'
 
 RSpec.describe Node do 
   it "exists" do 
-    node = Node.new()
+    node = Node.new("plop")
     expect(node).to be_an_instance_of(Node)
   end
- 
+ it "has data" do 
+  node = Node.new("plop")
+  expect(node.data).to eq("plop")
+ end
+
 end
