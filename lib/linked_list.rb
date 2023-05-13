701452bca0b6c692ee7c920a81aebca170c4f724
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-13 14:11:49 -0600

    Adds forgotten setter method to append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,28 +1,41 @@
 class Linked_list 
   attr_reader :head, :tail, :data
+  attr_writer :head
+
   def initialize
-    @head = nil
-    @tail = nil
+  self.head = nil
   end
 
   def append(data)
-    if(@head.nil?)
-      @head = Node.new(data, nil)
+    if(self.head.nil?)
+      self.head = Node.new(data, nil)
     else
-      lastNode = @head
+      lastNode = self.head
       while(!lastNode.next_node.nil?)
         lastNode = lastNode.next_node
       end
+      lastNode.next_node = Node.new(data, nil)
     end
   end
 
   def count 
-    if @head.nil?
+    if self.head.nil?
       0
     else
       1
     end
   end
+ 
+  def to_s 
+    beats = []
+    current_node = self.head
+    until current_node == nil
+      beats << current_node.data
+      current_node = current_node.next_node
+    end
+    beats.join(" ")
+  end
+
   # def prepend(data)
   #   @head = Node.new(data, @head)
   # end
