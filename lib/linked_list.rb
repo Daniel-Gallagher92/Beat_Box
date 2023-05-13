4249e2febe95832c5648ae47a2eb8f2e2083c922
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-12 21:04:15 -0600

    Adds append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,23 @@
-class Linked_list
-  attr_reader :head
-  def initialize(head)
+class Linked_list 
+  attr_reader :head, :tail, :data
+  def initialize
     @head = nil
+    @tail = nil
   end
+
+  def append(data)
+    if(@head.nil?)
+      @head = Node.new(data, nil)
+    else
+      lastNode = @head
+      while(!lastNode.next_node.nil?)
+        lastNode = lastNode.next_node
+      end
+    end
+  end
+
+
+  # def prepend(data)
+  #   @head = Node.new(data, @head)
+  # end
 end
