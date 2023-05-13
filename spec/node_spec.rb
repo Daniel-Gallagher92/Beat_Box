12ea6650ee5f5d495eec8fcd84e7a52785b89f4b
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-13 16:12:41 -0600

    Adds proper spacing to node tests

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe Node do
     node = Node.new("plop", nil)
     expect(node).to be_an_instance_of(Node)
   end
+
  it "has data" do 
   node = Node.new("plop", nil)
   expect(node.data).to eq("plop")
  end
+
  it "has next_node" do 
   node = Node.new("plop", nil)
   expect(node.next_node).to eq(nil)
