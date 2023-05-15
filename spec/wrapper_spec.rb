f85ca91ef7a31a34f337cdeaa16da4cbb8089288
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-15 17:00:53 -0600

    Test: Play method

--- spec/wrapper_spec.rb
+++ spec/wrapper_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe BeatBox do
   it "list exists" do 
     bb = BeatBox.new 
     list = Linked_list.new
-  # require 'pry'; binding.pry
 
     expect(bb.list).to be_an_instance_of(Linked_list)
   end
@@ -21,7 +20,6 @@ RSpec.describe BeatBox do
   it "can append" do 
     bb = BeatBox.new 
     list = Linked_list.new
-  # require 'pry'; binding.pry
 
     expect(bb.append("deep doo ditt")).to eq("deep doo ditt")
   end
@@ -37,7 +35,16 @@ RSpec.describe BeatBox do
 
     expect(bb.count).to eq(5)
   end
+  
+  it "can talk like hooman" do 
+    bb = BeatBox.new 
 
+
+    bb.append("woo hoo shu")
+  
+
+    expect(bb.play).to eq('say -r 150 -v Trinoids "woo hoo shu" ')
+  end
 
   
 end
